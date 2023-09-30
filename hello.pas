f6e4302ddbd2138efa15f386985a8345a9e14b35
Лужняк Александр <Lyzhniak2005@yandex.ru>
2023-09-30 10:22:23 +0300

    День/ночь

--- hello.pas
+++ hello.pas
@@ -1,3 +1,16 @@
 ﻿begin
-  print('Hello world!');
+  var hour := readinteger('Enter hour(0..23):');
+  assert( (hour >=0) and (hour<=23)); 
+  case hour of
+    4..10: print('Good morning, world!');
+    11..16: print('Good evening, world!');
+    17..22: print('Good night, world!');
+    
+    
+  end;
+  
+  
+  
+  
+  
 end.
